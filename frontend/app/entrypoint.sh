96ed11cb225939aba62f8914bd99a48507bb29bd
Said Oudrhich <said.oudrhich@educa.madrid.org>
2026-05-03 23:27:32 +0200

    fix(frontend): add ipv6only=on and mime.types to nginx

--- frontend/app/entrypoint.sh
+++ frontend/app/entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 cat > /etc/nginx/conf.d/default.conf << EOF
 server {
     listen ${PORT};
-    listen [::]:${PORT};
+    listen [::]:${PORT} ipv6only=on;
     root /usr/share/nginx/html;
     index index.html;
+    include /etc/nginx/mime.types;
     location / {
         try_files \$uri \$uri/ /index.html;
     }
